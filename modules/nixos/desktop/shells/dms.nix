fe2865256a252e1d6d868047f4a6e681400e7019
Huanlan233 <huanlan233@outlook.com>
2026-08-04 22:47:38 +0800

    fix: fcitx5 icon was missing in the DMS bar

--- modules/nixos/desktop/shells/dms.nix
+++ modules/nixos/desktop/shells/dms.nix
@@ -1,25 +1,44 @@
-{ config, lib, inputs, ... }:
+{ config, lib, inputs, pkgs, ... }:
 
 let
-    shell = config.settings.nixos.desktop.shell;
+  shell = config.settings.nixos.desktop.shell;
+
+  patchedDms = pkgs.dms-shell.overrideAttrs (old:
+    let
+      patchedSrc = pkgs.applyPatches {
+        name = "dms-shell-systemtray-icon";
+        src = old.src;
+        patches = [ ../../../../patches/dms-systemtray-icon-resolution.patch ];
+      };
+    in
+    {
+      src = patchedSrc;
+      sourceRoot = "${patchedSrc.name}/core";
+      postInstall = builtins.replaceStrings [
+        old.src.outPath
+      ] [
+        patchedSrc.outPath
+      ] old.postInstall;
+    });
 in
 {
-    imports = [
-        inputs.dms.nixosModules.dank-material-shell
-    ];
+  imports = [
+    inputs.dms.nixosModules.dank-material-shell
+  ];
 
-    config = lib.mkIf (shell.enable && shell.type == "dms") {
-        programs.dms-shell = {
-            enable = true;
-            systemd = {
-                enable = true;
-                restartIfChanged = true;
-            };
-            enableVPN = true;
-            enableSystemMonitoring = true;
-            enableDynamicTheming = true;
-            enableAudioWavelength = true;
-            enableCalendarEvents = true;
-        };
+  config = lib.mkIf (shell.enable && shell.type == "dms") {
+    programs.dms-shell = {
+      enable = true;
+      package = patchedDms;
+      systemd = {
+        enable = true;
+        restartIfChanged = true;
+      };
+      enableVPN = true;
+      enableSystemMonitoring = true;
+      enableDynamicTheming = true;
+      enableAudioWavelength = true;
+      enableCalendarEvents = true;
     };
+  };
 }
